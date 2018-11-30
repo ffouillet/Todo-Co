55e2449b2dd7e9f98cb5f53f3986c8f17834e4b5
ffouillet <felix.fouillet@gmail.com>
2018-11-30 11:28:53 +0100

    Added tests on entities. Finalized unit and functional tests. This closes #6.

---
+++ tests/Entity/TaskTest.php
@@ -0,0 +1,32 @@
+<?php
+namespace Tests\AppBundle\Entity;
+
+use App\Entity\Task;
+use App\Entity\User;
+use PHPUnit\Framework\TestCase;
+
+class TaskTest extends TestCase
+{
+    public function testAttributes()
+    {
+        $now = new \DateTime('NOW');
+
+        $userStub = $this->createMock(User::class);
+        $userStub->method('getId')
+            ->willReturn(0);
+
+        $task = new Task();
+        $task->setTitle('Unit Tested Task');
+        $task->setContent('Unit Tested Task content');
+        $task->setCreatedAt($now);
+        $task->toggle(true);
+        $task->setAuthor($userStub);
+
+        static::assertNull($task->getId());
+        static::assertEquals('Unit Tested Task', $task->getTitle());
+        static::assertEquals('Unit Tested Task content', $task->getContent());
+        static::assertEquals($now, $task->getCreatedAt());
+        static::assertEquals(true, $task->isDone());
+        static::assertEquals(0, $task->getAuthor()->getId());
+    }
+}
